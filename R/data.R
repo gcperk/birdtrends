26abd619eca50abdb04b27ffb79013f077c179c1
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-03-27 09:55:36 -0700

    document dataset

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Pacific Wren annual indices from 1968 to 2022
+#'
+#' An example of annual indices for Pacific Wren
+#'
+#' @format ## `annual_indicies_data`
+#' A tibble with 55 rows and 4 columns:
+#' \describe{
+#'   \item{year}{year of indices}
+#'   \item{index}{annual index}
+#'   \item{index_q_0.025}{confidence interval 2.5%}
+#'   \item{index_q_0.975}{confidence interval 97.5%}
+#'   ...
+#' }
+#' @source <https://github.com/bbsBayes/bbsBayes2/tree/main>
+"annual_indicies_data"
