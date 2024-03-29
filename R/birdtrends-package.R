a57f86de8858061b8ff746a3ec9bae85900915cf
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-03-29 12:59:32 -0700

    update specific imports

--- R/birdtrends-package.R
+++ R/birdtrends-package.R
@@ -2,5 +2,13 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom broom tidy
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 ggplot
+#' @importFrom purrr map
+#' @importFrom purrr map_dbl
+#' @importFrom stats lm
+#' @importFrom tibble tibble
+#' @importFrom utils data
 ## usethis namespace: end
 NULL
