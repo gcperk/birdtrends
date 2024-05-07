8b465bb73a25940ea1b1d1199f1476bf052f8650
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-05-06 18:03:51 -0700

    update tests

--- tests/testthat/test-get_trend.R
+++ tests/testthat/test-get_trend.R
@@ -4,9 +4,10 @@ test_that("fit_trend results correct", {
   expect_equal(length(tr$draw), 10)
 })
 
-test_that("fit_hgam fails appropriately", {
-  #results = fit_hgam(readRDS(test_path("testdata", "input1_test.rds")))
+test_that("fit_trend fails appropriately", {
+  trr = readRDS(test_path("testdata", "fit_hgam_model.rds"))
   expect_error(get_trend("a"))
-})
+  expect_error(get_trend(trr, method = "NA"))
+ })
 
 #testthat::test_file("tests/testthat/test-get_trend.R")
