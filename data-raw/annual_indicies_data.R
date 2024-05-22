e34e4886787bb298ae6847f4da8f867ecbc3b240
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-05-21 21:38:02 -0700

    update file paht

--- data-raw/annual_indicies_data.R
+++ data-raw/annual_indicies_data.R
@@ -15,7 +15,7 @@ aou <- bbsBayes2::search_species("Pacific Wren")$aou[1]
 
 # loading the fitted model object
 #fit <- readRDS(paste0("C:/GitHub/CWS_2022_BBS_Analyses/output/fit_",aou,".rds"))
-fit <- readRDS(paste0("C:/r_repo/2024_ECCC_birdtrends/birdtrends_pif/fitted_models/fit_",aou,".rds"))
+fit <- readRDS(paste0("C:/r_repo/2024_ECCC_birdtrends/birdtrends_pif/01_fitted_models/fit_",aou,".rds"))
 
 
 indsmooth <- generate_indices(fit,regions = "country",
