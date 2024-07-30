1eb9b72e71ede4d27b6c1532f49dc3d83735e034
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-07-30 15:46:46 -0700

    document dataset

--- R/data.R
+++ R/data.R
@@ -13,3 +13,38 @@
 #' }
 #' @source <https://github.com/bbsBayes/bbsBayes2/tree/main>
 "annual_indicies_data"
+
+
+#' Pacific Wren bbs Smooth output from 1968 to 2022
+#'
+#' An example of posterior draws from smooth component of BBS model for Pacific Wren
+#'
+#' @format ## `bbs_smooth_data`
+#' A tibble with 55 rows and 4 columns:
+#' \describe{
+#'   \item{year}{year of indices}
+#'   \item{index}{annual index}
+#'   \item{index_q_0.025}{confidence interval 2.5%}
+#'   \item{index_q_0.975}{confidence interval 97.5%}
+#'   ...
+#' }
+#' @source <https://github.com/bbsBayes/bbsBayes2/tree/main>
+"bbs_smooth_data"
+
+
+#' Pacific Wren posterior draws output from 1968 to 2022
+#'
+#' An example of posterior draws for Pacific Wren
+#'
+#' @format ## `posterior_draws_data`
+#' A tibble with 55 rows and 4 columns:
+#' \describe{
+#'   \item{year}{year of indices}
+#'   \item{index}{annual index}
+#'   \item{index_q_0.025}{confidence interval 2.5%}
+#'   \item{index_q_0.975}{confidence interval 97.5%}
+#'   ...
+#' }
+#' @source <https://github.com/bbsBayes/bbsBayes2/tree/main>
+"posterior_draws_data"
+
